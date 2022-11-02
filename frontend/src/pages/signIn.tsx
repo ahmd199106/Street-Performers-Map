9875399cc7cb21e198b60e3f24c155e5f8d1a8b8
MohammedAK1991 <ahmd199106@gmail.com>
2022-11-02 23:46:23 +0000

    Basic signIn and SignUp routes

---
+++ frontend/src/pages/signIn.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+type signInProps = {};
+
+const signIn: React.FC<signInProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default signIn;
---
+++ frontend/src/pages/signUp.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+type signUpProps = {};
+
+const signUp: React.FC<signUpProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default signUp;

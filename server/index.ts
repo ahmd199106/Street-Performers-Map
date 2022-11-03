939bf54ad1779e4062b4a128bf90556c2210769d
MohammedAK1991 <ahmd199106@gmail.com>
2022-11-03 14:14:25 +0000

    install typescript in the express project

---
+++ server/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors()); // Use the cors middleware
+
+
+app.get("/", (req, res) => {
+  res.send('Express independent Backend is working ');
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
